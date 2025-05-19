0faf708b5be63e38e8f333c006336c456df6947c
Sachiko07 <95449474+Sachiko07@users.noreply.github.com>
2025-05-19 10:53:13 +0000

    add file

--- practice_tasks/practice_tasks.py
+++ practice_tasks/practice_tasks.py
@@ -2,3 +2,18 @@ sensor_ID = "6001"
 sensor_type = "temeperature"
 location = "Cage_01"
 
+""" Variables do not need to 
+be declared with any particular type, 
+and can even change type after they have been set """
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+"""variables are case sensitive"""
+"""This will create two variables"""
+
+a = 4
+A = "Sally"
+
+
